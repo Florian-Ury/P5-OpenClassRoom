f3b24383d985b949391d8babdd23e674f99439a3
Florian-Ury <Florian.heraud.78@gmail.com>
2022-11-18 10:37:46 +0100

    Request POST réparée, total qty & price on removeStorage, orderID on confirmation.js

---
+++ front/js/validation.js
@@ -0,0 +1,72 @@
+// Récupération des ID pour l'event
+let formInputs = new Array();
+
+formInputs.push(document.querySelector('#firstName'));
+formInputs.push(document.querySelector('#lastName'));
+formInputs.push(document.querySelector('#address'));
+formInputs.push(document.querySelector('#city'));
+formInputs.push(document.querySelector('#email'));
+
+
+
+/**
+ * add event listener on all input form elements
+ */
+for(let i =0; i < formInputs.length; i++) {
+
+    let input = formInputs[i];
+    input.addEventListener('change', function() {
+        checkValidElement(this);
+    })
+}
+
+/**
+ * function check all input form element
+ */
+const validForm = () => {
+    for(let i =0; i < formInputs.length; i++) {
+        let input = formInputs[i];
+        checkValidElement(input);
+    }
+}
+
+
+/**
+ * function check element
+ * @param element
+ */
+const checkValidElement = (element) => {
+    let result = true;
+    let number = false;
+    let type = element.type;
+    let name = element.name;
+    let string = element.value;
+    let targetDiv = element.nextElementSibling;
+
+    //valid text
+    if(type == "text") {
+        // verif if name == adress
+        if(name == "address"){
+            number = true;
+        }
+
+        if(!validText(string, number)){
+            result = false;
+        }
+    }
+
+    if(type == "email") {
+        if(!validEmail(string)){
+            result = false;
+        }
+    }
+
+    // show text
+    if(result == false) {
+        targetDiv.textContent = "Veuillez entrer une donnée valide";
+    }
+
+     return result
+}
+
+
